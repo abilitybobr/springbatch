a21269732c4fcb11cd40e67f98931d5fb61c26d7
SONGYOUNGJUN <songyjun@cj.net>
2023-09-21 09:56:14 +0900

    Git Revert test1

--- src/main/java/com/study/springbatch/domain/job/StudyBatchOneJobConfig.java
+++ src/main/java/com/study/springbatch/domain/job/StudyBatchOneJobConfig.java
@@ -42,4 +42,12 @@ public class StudyBatchOneJobConfig {
                 .tasklet(new StudyOneTasklet(studyBatchOneService))
                 .build();
     }
+
+    public void revert1() {
+
+    }
+
+    public void revert2() {
+
+    }
 }
